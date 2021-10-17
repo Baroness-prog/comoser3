54e7e3fd8cb16d565d02fa839c98d9d358e95b86
Aurélien <reyaurelien.business@gmail.com>
2021-10-17 22:20:53 +0200

    add autoloaload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'e07b242fe58092414c7b575f78815a31957cba1e',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'e07b242fe58092414c7b575f78815a31957cba1e',
+            'dev_requirement' => false,
+        ),
+        'gipetto/cowsay' => array(
+            'pretty_version' => '1.1.0',
+            'version' => '1.1.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../gipetto/cowsay',
+            'aliases' => array(),
+            'reference' => '85218ee4c0d18cc8753e08af54a8740aac12860a',
+            'dev_requirement' => false,
+        ),
+    ),
+);
